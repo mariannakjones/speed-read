aa7bb2996e0ae27e2d6c1e1de5e539ff45c8e037
mariannakjones <69316131+mariannakjones@users.noreply.github.com>
2022-05-23 12:06:04 -0400

    Add files via upload

---
+++ speed-read.js
@@ -0,0 +1,40 @@
+var elements = document.querySelectorAll(`p, h1, h2, h3, h4, h5, h6, ul`);
+var hyperlinks = document.getElementsByTagName(`a`);
+
+
+for (let i=0; i<elements.length; i++) {
+    var text = elements[i].innerText;
+    if (text !== ``) {
+        const words = text.match(/(?<!')\b[^\d\W]+\b/g);
+        for (let j in words) {
+            var pattern = `(?<!<|")\\b` + words[j] + `\\b(?!>)`;
+            var regex = new RegExp(pattern, `g`);
+            elements[i].innerHTML = elements[i].innerHTML.replace(regex, boldString(words[j]));
+        }
+    }
+}
+
+for (let i=0; i<hyperlinks.length; i++) {
+    var regex = /<b>|<\/b>/g;
+    var href = hyperlinks[i].getAttribute(`href`);
+    var title = hyperlinks[i].getAttribute(`title`); 
+    hyperlinks[i].href = href.replace(regex, ``);
+    if (title !== null) {
+        hyperlinks[i].title = title.replace(regex, ``);
+    }
+}
+
+
+function boldString(str){
+    switch(str.length){
+        case 1:
+        case 2:
+        case 3:
+            return str.slice(0, 1).bold() + str.slice(1);
+        case 4:
+            return str.slice(0, 2).bold() + str.slice(2);
+        default:
+            var lengthBold = Math.round(str.length * 0.6);
+            return str.slice(0, lengthBold).bold() + str.slice(lengthBold);
+    }
+}
